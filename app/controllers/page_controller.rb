7a36eafc860446626a8a19bd010be704191523e1
laineycos <elainecosgrove1@gmail.com>
2016-04-27 18:17:03 +0000

    re-commit

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,16 @@
+class PageController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :about, :faqs] #restric action for user on some pages or use "only:" for specifying 1 action for user to be banned
+  
+  def home
+  end
+
+  def about
+  end
+
+  def faqs
+  end
+
+  def contact
+  end
+  
+end
